fff9a5a0423b2a77d914c9dd7ed89938c0268b55
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-18 18:34:29 +0100

    Enable mic permission

--- src/StftPitchShiftPlugin/Plugin.cmake
+++ src/StftPitchShiftPlugin/Plugin.cmake
@@ -1,18 +1,19 @@
 juce_add_plugin(${CMAKE_PROJECT_NAME}
-  PRODUCT_NAME             "${CMAKE_PROJECT_NAME}"
-  VERSION                  "${CMAKE_PROJECT_VERSION}"
-  COMPANY_NAME             "Juergen Hock"
-  COMPANY_EMAIL            "juergen.hock@jurihock.de"
-  COMPANY_WEBSITE          "https://github.com/jurihock/stftPitchShiftPlugin"
-  BUNDLE_ID                "de.jurihock.stftpitchshift"
-  PLUGIN_MANUFACTURER_CODE Hock
-  PLUGIN_CODE              Stft
-  IS_SYNTH                 FALSE
-  IS_MIDI_EFFECT           FALSE
-  NEEDS_MIDI_INPUT         FALSE
-  NEEDS_MIDI_OUTPUT        FALSE
-  COPY_PLUGIN_AFTER_BUILD  FALSE
-  FORMATS                  AU VST3 Standalone)
+  PRODUCT_NAME                  "${CMAKE_PROJECT_NAME}"
+  VERSION                       "${CMAKE_PROJECT_VERSION}"
+  COMPANY_NAME                  "Juergen Hock"
+  COMPANY_EMAIL                 "juergen.hock@jurihock.de"
+  COMPANY_WEBSITE               "https://github.com/jurihock/stftPitchShiftPlugin"
+  BUNDLE_ID                     "de.jurihock.stftpitchshift"
+  PLUGIN_MANUFACTURER_CODE      Hock
+  PLUGIN_CODE                   Stft
+  IS_SYNTH                      FALSE
+  IS_MIDI_EFFECT                FALSE
+  NEEDS_MIDI_INPUT              FALSE
+  NEEDS_MIDI_OUTPUT             FALSE
+  MICROPHONE_PERMISSION_ENABLED TRUE
+  COPY_PLUGIN_AFTER_BUILD       FALSE
+  FORMATS                       AU VST3 Standalone)
 
 juce_generate_juce_header(${CMAKE_PROJECT_NAME})
 
