9a5e9c64d7db98061d3de0177040d91cf631033f
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-25 13:55:16 +0100

    Try os matrix

--- scripts/ubuntu_build.sh
+++ scripts/ubuntu_build.sh
@@ -7,5 +7,9 @@ CONFIG=Release
 INPUT=${ROOT}
 OUTPUT=${ROOT}/build
 
+ARTEFACTS=${OUTPUT}/StftPitchShiftPlugin_artefacts/${CONFIG}
+
 cmake -DCMAKE_BUILD_TYPE=${CONFIG} -S "${INPUT}" -B "${OUTPUT}" || exit /b
 cmake --build "${OUTPUT}" || exit /b
+
+ls -m "${ARTEFACTS}"
