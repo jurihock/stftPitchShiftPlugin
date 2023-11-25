3c681e2181ceefb5e343acc69d088bd9a2765611
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-25 13:22:54 +0100

    Update debian_build.sh

--- scripts/debian_build.sh
+++ scripts/debian_build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BASE = $(dirname "$(readlink -f "$0")")
-ROOT = $(dirname "${BASE}")
+BASE=$(dirname "$(readlink -f "$0")")
+ROOT=$(dirname "${BASE}")
 
-CONFIG = Release
-INPUT  = ${ROOT}
-OUTPUT = ${ROOT}/build
+CONFIG= Release
+INPUT=${ROOT}
+OUTPUT=${ROOT}/build
 
 cmake -DCMAKE_BUILD_TYPE=${CONFIG} -S "${INPUT}" -B "${OUTPUT}" || exit /b
 cmake --build "${OUTPUT}" || exit /b
