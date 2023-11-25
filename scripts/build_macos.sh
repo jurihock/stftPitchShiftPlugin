b6ae99d1126a92088f1c9621707a78526341a9d9
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-25 14:05:29 +0100

    Add MacOS

---
+++ scripts/build_macos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE=$(dirname "$(readlink -f "$0")")
+ROOT=$(dirname "${BASE}")
+
+ARGS=-DCMAKE_OSX_ARCHITECTURES=arm64
+CONFIG=Release
+INPUT=${ROOT}
+OUTPUT=${ROOT}/build
+
+ARTEFACTS=${OUTPUT}/StftPitchShiftPlugin_artefacts/${CONFIG}
+
+cmake ${ARGS} -DCMAKE_BUILD_TYPE=${CONFIG} -S "${INPUT}" -B "${OUTPUT}" || exit /b
+cmake --build "${OUTPUT}" || exit /b
+
+ls -m "${ARTEFACTS}"
--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -3,13 +3,14 @@
 BASE=$(dirname "$(readlink -f "$0")")
 ROOT=$(dirname "${BASE}")
 
+ARGS=
 CONFIG=Release
 INPUT=${ROOT}
 OUTPUT=${ROOT}/build
 
 ARTEFACTS=${OUTPUT}/StftPitchShiftPlugin_artefacts/${CONFIG}
 
-cmake -DCMAKE_BUILD_TYPE=${CONFIG} -S "${INPUT}" -B "${OUTPUT}" || exit /b
+cmake ${ARGS} -DCMAKE_BUILD_TYPE=${CONFIG} -S "${INPUT}" -B "${OUTPUT}" || exit /b
 cmake --build "${OUTPUT}" || exit /b
 
 ls -m "${ARTEFACTS}"
