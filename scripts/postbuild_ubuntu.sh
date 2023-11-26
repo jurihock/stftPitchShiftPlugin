be1943e17ef78d4749818e3aefaafee850f125ea
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-26 16:14:51 +0100

    Pass SKIP_GUI_TESTS to the child process

--- scripts/postbuild_ubuntu.sh
+++ scripts/postbuild_ubuntu.sh
@@ -14,6 +14,6 @@ PLUGINVAL=${OUTPUT}/_deps/pluginval-src/pluginval
 # If specified, avoids tests that create GUI windows,
 # which can cause problems on headless CI systems.
 # https://github.com/Tracktion/pluginval/blob/develop/docs/Command%20line%20options.md
-SKIP_GUI_TESTS=1
+export SKIP_GUI_TESTS=1
 
 "${PLUGINVAL}" ${ARGS} "${ARTEFACTS}/VST3/StftPitchShiftPlugin.vst3" || exit $?
