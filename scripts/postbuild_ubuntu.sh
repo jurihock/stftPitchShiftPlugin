c6b46085c15026ab59a84027ac789368b81ad803
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-26 15:57:47 +0100

    Try to fix ubuntu test

--- scripts/postbuild_ubuntu.sh
+++ scripts/postbuild_ubuntu.sh
@@ -11,4 +11,9 @@ OUTPUT=${ROOT}/build
 ARTEFACTS=${OUTPUT}/StftPitchShiftPlugin_artefacts/${CONFIG}
 PLUGINVAL=${OUTPUT}/_deps/pluginval-src/pluginval
 
+# If specified, avoids tests that create GUI windows,
+# which can cause problems on headless CI systems.
+# https://github.com/Tracktion/pluginval/blob/develop/docs/Command%20line%20options.md
+SKIP_GUI_TESTS=1
+
 "${PLUGINVAL}" ${ARGS} "${ARTEFACTS}/VST3/StftPitchShiftPlugin.vst3" || exit $?
