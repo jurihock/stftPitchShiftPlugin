680ccd5d8e1d79be8c9bc4f262bc5c8964f0d62a
Jürgen Hock <juergen.hock@jurihock.de>
2026-07-19 09:21:01 +0200

    Update JUCE v8 build dependencies

--- scripts/prebuild_ubuntu.sh
+++ scripts/prebuild_ubuntu.sh
@@ -6,4 +6,11 @@ sudo apt update
 sudo apt install --yes build-essential cmake pkg-config
 
 # https://github.com/juce-framework/JUCE/blob/master/docs/Linux%20Dependencies.md
-sudo apt install --yes libasound2-dev libjack-jackd2-dev ladspa-sdk libcurl4-openssl-dev libfreetype6-dev libx11-dev libxcomposite-dev libxcursor-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev mesa-common-dev
+sudo apt install --yes \
+  libasound2-dev libjack-jackd2-dev \
+  ladspa-sdk \
+  libcurl4-openssl-dev  \
+  libfreetype-dev libfontconfig1-dev \
+  libx11-dev libxcomposite-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev \
+  libwebkit2gtk-4.1-dev \
+  libglu1-mesa-dev mesa-common-dev
