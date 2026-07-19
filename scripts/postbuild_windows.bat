cab1cd2d5817d06977a19e4e4baa2d6ba3647f92
Jürgen Hock <juergen.hock@jurihock.de>
2026-07-19 12:00:30 +0200

    Zip windows binaries in the same way as in macos and ubuntu builds

--- scripts/postbuild_windows.bat
+++ scripts/postbuild_windows.bat
@@ -16,8 +16,20 @@ set OUTPUT=%ROOT%\build
 set ARTEFACTS=%OUTPUT%\StftPitchShiftPlugin_artefacts\%CONFIG%
 set PLUGINVAL=%OUTPUT%\_deps\pluginval-src\pluginval.exe
 
+rem Execute pluginval.
 "%PLUGINVAL%" %ARGS% "%ARTEFACTS%\VST3\StftPitchShiftPlugin.vst3" || exit /b
 
+rem Delete intermediate files.
 del /q "%ARTEFACTS%\StftPitchShiftPlugin_SharedCode.lib"
 del /q "%ARTEFACTS%\VST3\StftPitchShiftPlugin.exp"
 del /q "%ARTEFACTS%\VST3\StftPitchShiftPlugin.lib"
+
+rem # Zip windows binaries in the same way as in macos and ubuntu builds.
+pushd "%ARTEFACTS%"
+tar -caf "..\StftPitchShiftPlugin.zip" -C "%ARTEFACTS%" . || exit /b
+popd
+rmdir /s /q "%ARTEFACTS%" || exit /b
+mkdir "%ARTEFACTS%" || exit /b
+pushd "%ARTEFACTS%"
+move "..\StftPitchShiftPlugin.zip" . || exit /b
+popd
